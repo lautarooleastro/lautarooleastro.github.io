9afa4353418d255ab5d6a4de05e8797b54242e6b
Lautaro Oleastro <lauchaoleastro1@gmail.com>
2024-01-03 16:33:05 -0300

    transition finished

--- static/main.js
+++ static/main.js
@@ -1,7 +1,7 @@
+const loadingScreenElement = document.getElementById('loadingScreen');
+const homeScreenElement = document.getElementById('homeScreen');
 
-
-document.addEventListener("DOMContentLoaded", function() {
-  var loadingScreenElement = document.getElementById('loadingScreen');
-
+window.addEventListener("load", function() {
   loadingScreenElement.classList.add('fade-out-animation');
+  homeScreenElement.classList.add('fade-in-animation');
 });
