757c93e7bd0a878a016a7b8d85668181e6fb4cb5
Lautaro Oleastro <lauchaoleastro1@gmail.com>
2024-01-03 15:44:07 -0300

    loading screen animation

---
+++ static/main.js
@@ -0,0 +1,7 @@
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  var loadingScreenElement = document.getElementById('loadingScreen');
+
+  loadingScreenElement.classList.add('fade-out-animation');
+});
