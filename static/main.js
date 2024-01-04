531603ba7582eafbb109203163ec4cbdfdf030a9
Lautaro Oleastro <lauchaoleastro1@gmail.com>
2024-01-04 15:40:50 -0300

    checkpoint to see on phone

--- static/main.js
+++ static/main.js
@@ -1,6 +1,8 @@
 const loadingScreenElement = document.getElementById('loadingScreen');
 const homeScreenElement = document.getElementById('homeScreen');
 
+window.scrollTo(0, 0);
+
 window.addEventListener("load", function() {
   loadingScreenElement.classList.add('fade-out-animation');
   homeScreenElement.classList.add('fade-in-animation');
